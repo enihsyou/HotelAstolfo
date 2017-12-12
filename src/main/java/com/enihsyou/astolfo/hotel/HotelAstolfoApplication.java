3f397fae46c718b64d8278fb91544960c1e36b89
九条涼果 <s1131234@gmail.com>
2017-12-12 21:05:15 +0800

    添加REST Docs

--- src/main/java/com/enihsyou/astolfo/hotel/HotelAstolfoApplication.java
+++ src/main/java/com/enihsyou/astolfo/hotel/HotelAstolfoApplication.java
@@ -1,25 +1,12 @@
 package com.enihsyou.astolfo.hotel;
 
-import com.enihsyou.astolfo.hotel.mybatis.UserMapper;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HotelAstolfoApplication implements CommandLineRunner {
+public class HotelAstolfoApplication{
 
   public static void main(String[] args) {
     SpringApplication.run(HotelAstolfoApplication.class, args);
-  }
-
-  final private UserMapper userMapper;
-
-  public HotelAstolfoApplication(UserMapper userMapper) {
-    this.userMapper = userMapper;
-  }
-
-  @Override
-  public void run(String... args) {
-    System.out.println(this.userMapper.selectByUsername("dfd"));
   }
 }
