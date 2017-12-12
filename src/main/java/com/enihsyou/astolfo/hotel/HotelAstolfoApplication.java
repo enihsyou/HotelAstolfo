922274fe6a6f4127dc4f8d11eb8939ea321bc852
九条涼果 <s1131234@gmail.com>
2017-12-12 17:33:01 +0800

    更正文件目录

--- src/main/java/com/enihsyou/astolfo/hotel/HotelAstolfoApplication.java
+++ src/main/java/com/enihsyou/astolfo/hotel/HotelAstolfoApplication.java
@@ -19,7 +19,7 @@ public class HotelAstolfoApplication implements CommandLineRunner {
   }
 
   @Override
-  public void run(String... args) throws Exception {
+  public void run(String... args) {
     System.out.println(this.userMapper.selectByUsername("dfd"));
   }
 }
--- src/main/kotlin/com/enihsyou/astolfo/hotel/mybatis/UserMapper.kt
+++ src/main/kotlin/com/enihsyou/astolfo/hotel/mybatis/UserMapper.kt
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Select
 
 @Mapper
 interface UserMapper {
+
   @Select("SELECT * FROM student WHERE student_name = #{username}")
   fun selectByUsername(username: String): User
 }
