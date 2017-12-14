518b2ce28f2951105a46c970c52e3f9890254eee
九条涼果 <s1131234@gmail.com>
2017-12-14 13:06:55 +0800

    sync

---
+++ src/main/resources/hotel_select3.sql
@@ -0,0 +1,30 @@
+--更具手机号获取职员职位
+SELECT authority FROM authorities where phone_number=#{phone_number};
+
+--更具手机号获取登录密码，用于登录
+SELECT password FROM registered_user WHERE phone_number=#{phone_number};
+--同上，使用nickname  修改一下DDL 其中nickname我忘记写unique了
+SELECT password FROM  registered_user WHERE nickname=#{nickname};
+
+--获取表的所有信息
+SELECT * FROM registered_user WHERE nickname=#{nickname};
+SELECT * FROM registered_user WHERE phone_number = #{phone_number};
+SELECT * FROM room_type;
+
+
+
+--查询适合的房间
+SELECT room_id FROM rooms WHERE room_left=TRUE AND room_direction=#{room_direction} AND room_type_id=#{room_type_id};
+SELECT room_id FROM rooms WHERE room_left=TRUE AND room_direction=#{room_direction};
+
+
+--根据手机号查询旅客定的房间
+SELECT room_id FROM bookings WHERE phone_number=#{phone_number};
+
+
+--根据订房的编号查询旅客定的房间
+SELECT room_id FROM bookings WHERE booking_id=#{booking_id};
+
+-- 查询某日有哪些空房间可以预定
+SELECT room_id FROM  rooms WHERE room_id NOT IN
+  (SELECT room_id FROM bookings WHERE #{date}>bookings.date_to AND #{date}<=bookings.date_from);
