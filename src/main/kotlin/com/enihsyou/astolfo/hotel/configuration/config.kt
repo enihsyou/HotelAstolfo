ce4d885bebd48c195c547d24e280b8e41a9b8d66
九条涼果 <s1131234@gmail.com>
2017-12-25 10:18:43 +0800

    CORS

--- src/main/kotlin/com/enihsyou/astolfo/hotel/configuration/config.kt
+++ src/main/kotlin/com/enihsyou/astolfo/hotel/configuration/config.kt
@@ -1,5 +1,19 @@
 package com.enihsyou.astolfo.hotel.configuration
 
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+
+@Configuration
+@EnableWebMvc
+class WebConfig : WebMvcConfigurerAdapter() {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/api/**")
+    }
+}
 
 //@Configuration
 //@EnableWebSecurity
