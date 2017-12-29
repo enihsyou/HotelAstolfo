bcdde87b047479677afb5d26794177fcb46ca139
sorahjy <sorahjy@gmail.com>
2017-12-29 20:51:12 +0800

    Test demo v0.1

--- src/test/java/com/enihsyou/astolfo/hotel/controller/UserControllerTest.kt
+++ src/test/java/com/enihsyou/astolfo/hotel/controller/UserControllerTest.kt
@@ -1,27 +1,30 @@
 package com.enihsyou.astolfo.hotel.controller
 
-import org.apache.commons.lang3.StringUtils.containsOnly
-import org.hamcrest.CoreMatchers.equalTo
-import org.hamcrest.core.Is.`is`
-import org.junit.Before
-import org.junit.Rule
-import org.springframework.beans.factory.annotation.Autowired
+import io.restassured.RestAssured.given
+import org.hamcrest.Matchers.equalTo
 import org.testng.annotations.Test
-
-import io.restassured.RestAssured.*
-import io.restassured.matcher.RestAssuredMatchers.*
-import org.hamcrest.Matchers.*
-
 
 
 class UserControllerTest {
 
-    @Test
-    fun lotto_resource_returns_200_with_expected_id_and_winners() {
-
-        `when`().get("http://localhost:10080/api/rooms/")
-            .then().statusCode(200)
+//    @Test
+//    fun lotto_resource_returns_200_with_expected_id_and_winners() {
+//
+//        `when`().get("https://enihsyou.synology.me:8899/api/rooms/")
+//            .then().statusCode(200)
 //            .body("lotto.lottoId", equalTo(5),
 //            "lotto.winners.winnerId",equals(23))
+//    }
+    @Test
+    fun test(){
+        val bodyString= "{\"phoneNumber\": \"18800000001\",\"password\": \"2333\",\"nickname\": \"昵称\"}";
+        given().contentType("application/json").body(bodyString).
+        `when`().post("https://enihsyou.synology.me:8899/api/users/make").then().assertThat()
+                .body("id",equalTo(32))
+                .body("phoneNumber", equalTo("18800000001"))
+                .body("role", equalTo("注册用户"))
+                .statusCode(201)
     }
+
+
 }
