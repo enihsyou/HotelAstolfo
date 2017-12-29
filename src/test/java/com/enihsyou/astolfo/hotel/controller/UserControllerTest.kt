ebca166ed438d82729d3d9a9a6a3a0763c2665a4
sorahjy <sorahjy@gmail.com>
2017-12-29 21:48:37 +0800

    Test demo v0.11

--- src/test/java/com/enihsyou/astolfo/hotel/controller/UserControllerTest.kt
+++ src/test/java/com/enihsyou/astolfo/hotel/controller/UserControllerTest.kt
@@ -1,8 +1,8 @@
 package com.enihsyou.astolfo.hotel.controller
 
 import io.restassured.RestAssured.given
-import org.hamcrest.Matchers.equalTo
-import org.testng.annotations.Test
+import org.hamcrest.Matchers
+import org.junit.Test
 
 
 class UserControllerTest {
@@ -10,19 +10,23 @@ class UserControllerTest {
 //    @Test
 //    fun lotto_resource_returns_200_with_expected_id_and_winners() {
 //
-//        `when`().get("https://enihsyou.synology.me:8899/api/rooms/")
+//        `when`().get("http://localhost:10080/api/rooms/")
 //            .then().statusCode(200)
 //            .body("lotto.lottoId", equalTo(5),
 //            "lotto.winners.winnerId",equals(23))
 //    }
+
     @Test
-    fun test(){
-        val bodyString= "{\"phoneNumber\": \"18800000001\",\"password\": \"2333\",\"nickname\": \"昵称\"}";
+    fun test() {
+        val bodyString = "{\n  \"phoneNumber\": \"18800000001\",\n  \"password\": \"2333\",\n  \"nickname\": \"昵称\"\n}"
         given().contentType("application/json").body(bodyString).
-        `when`().post("https://enihsyou.synology.me:8899/api/users/make").then().assertThat()
-                .body("id",equalTo(32))
-                .body("phoneNumber", equalTo("18800000001"))
-                .body("role", equalTo("注册用户"))
+                `when`().post("https://enihsyou.synology.me:8899/api/users/make")
+                .then()
+                .also { it.log().all() }
+                .assertThat()
+                .body("id", Matchers.equalTo(32))
+                .body("phoneNumber", Matchers.equalTo("18800000001"))
+                .body("role", Matchers.equalTo("注册用户"))
                 .statusCode(201)
     }
 
