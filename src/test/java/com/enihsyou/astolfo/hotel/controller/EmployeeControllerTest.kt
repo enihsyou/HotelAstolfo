38a10e19f4450cd542aaa7d3a6f258493ce6579a
sorahjy <sorahjy@gmail.com>
2018-01-02 15:38:47 +0800

    Test v0.9 日常更新

--- src/test/java/com/enihsyou/astolfo/hotel/controller/EmployeeControllerTest.kt
+++ src/test/java/com/enihsyou/astolfo/hotel/controller/EmployeeControllerTest.kt
@@ -12,7 +12,7 @@ class EmployeeControllerTest internal constructor() {
     fun signin_with_employee_returns_201_with_expected_PhoneNumber_and_nickname() {
         val bodyString = "{\n  \"phoneNumber\": \"18019002573\",\n  \"password\": \"2333\",\n  \"nickname\": \"我是前台\"\n}"
         given().contentType("application/json").body(bodyString).
-                `when`().post("https://enihsyou.synology.me:8899/api/users/make")
+                `when`().post("https://astolfo.20001.me:443/api/users/make")
                 .then()
                 .also { it.log().all() }
                 .assertThat()
@@ -21,7 +21,7 @@ class EmployeeControllerTest internal constructor() {
 
     @Test
     fun delete_employee_returns_200_if_success() {
-        val postString = "https://enihsyou.synology.me:8899/api/users?phone=18019002573"
+        val postString = "https://astolfo.20001.me:443/api/users?phone=18019002573"
         given().delete(postString).then()
                 .also { it.log().all() }
                 .assertThat()
